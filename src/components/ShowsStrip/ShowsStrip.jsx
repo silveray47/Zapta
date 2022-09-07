b8843797d294cf59ded68e5b54c2db8b53a47aa1
silveray47 <76470712+silveray47@users.noreply.github.com>
2022-09-07 16:52:38 +0300

    basic carusela

--- src/components/ShowsStrip/ShowsStrip.jsx
+++ src/components/ShowsStrip/ShowsStrip.jsx
@@ -1,18 +1,62 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Card from '../card/Card';
+import Card from '../card/Card'
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 
-export default function ShowsStrip( {showList}) {
-    const Shows = [...showList]
 
-    return (
-        Shows.map((show,index)=>
-        <Link key={index} to={'/Concert/'+Object.values(show).join('')}> {/* ==> ? WTF*/ }
-            <Card showId={show}></Card>
-        </Link>
-
-        )
-
+function ShowsStrip() {
+    const responsive = {
+        superLargeDesktop: {
+          // the naming can be any, depends on you.
+          breakpoint: { max: 4000, min: 3000 },
+          items: 10
+        },
+        desktop: {
+          breakpoint: { max: 3000, min: 1024 },
+          items: 7
+        },
+        tablet: {
+          breakpoint: { max: 1024, min: 464 },
+          items: 2
+        },
+        mobile: {
+          breakpoint: { max: 464, min: 0 },
+          items: 1
+        }
+      };
+  return (
+    <div>
+        <Carousel 
+            responsive={responsive}
+            swipeable={false}
+            draggable={false}
+            showDots={true}
+            infinite={true}
+            // autoPlay={this.props.deviceType !== "mobile" ? true : false}
+            // autoPlaySpeed={1000}
+            // keyBoardControl={true}
+            // customTransition="all .5"
+            // transitionDuration={500}
+            // containerClass="carousel-container"
+            // removeArrowOnDeviceType={["tablet", "mobile"]}
+            // dotListClass="custom-dot-list-style"
+            // itemClass="carousel-item-padding-40-px"
+            >
+        <div><Card showId = '1' /></div>
+        <div><Card showId = '2' /></div>
+        <div><Card showId = '3' /></div>
+        <div><Card showId = '4' /></div>
+        <div><Card showId = '5' /></div>
+        <div><Card showId = '6' /></div>
+        <div><Card showId = '7' /></div>
+        <div><Card showId = '8' /></div>
+        <div><Card showId = '9' /></div>
+        <div><Card showId = '10' /></div>
+        </Carousel>;
+    </div>
   )
 }
+
+export default ShowsStrip
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -4,7 +4,8 @@ import './home.css'
 import concertsList from '../../concerts.json'
 import { Link } from 'react-router-dom'
 
-
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
 
 export default function Home() {
@@ -12,6 +13,11 @@ export default function Home() {
   const [godMusic, setGodMusic] = useState(['tamir','machina','charchOfTheMind'])
   return (
     <div className='home'>
+
+      <ShowsStrip />
+
+
+      
       <div>
           <h1>Come to Zappta ! </h1> <br />
           <div className="inner">
@@ -21,15 +27,14 @@ export default function Home() {
                   <div key={index} className="box">
                     <Link to={"/Concert/"+concert.id}><img src={concert.image}/></Link>
                     <br />
-
                     {concert.artist}
                   </div>
                 )
-
               })
             }
            </div>
       </div>
+
       {/* <div id='cards'>
         <div className='ShowsStripContainer'>
           <ShowsStrip clasName='ShowsStrip' showList={newShows} />
